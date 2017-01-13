68be3c842aca21d53896b3634c4b15b9dac45de6
Agnieszka Szczurek <agnieszka.szczurek@miquido.com>
2017-01-13 14:25:25 +0100

    First commit

---
+++ app/models/ticket.rb
@@ -0,0 +1,9 @@
+class Ticket < ActiveRecord::Base
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+  validates :name, length: {minimum: 6}, presence: true
+  validates :email_address, presence: true, format: { with: VALID_EMAIL_REGEX }
+  validates :price, presence: true
+  validates :seat_id_seq, length: {minimum: 2, maximum: 4}, presence: true
+end
+
