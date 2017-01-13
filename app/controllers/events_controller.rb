cd4295a2c8bbdf525839e5a2e36e5c9c03db998c
Agnieszka Szczurek <agnieszka.szczurek@miquido.com>
2017-01-13 18:25:40 +0100

    Create adding event.

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -4,8 +4,25 @@ class EventsController < ApplicationController
   end
 
   def new
+    @event = Event.new
   end
 
   def create
+
+    @event = Event.new(event_params)
+    if @event.save
+    flash[:komunikat] = 'Event został poprawnie stworzony.'
+    redirect_to events_path
+    else
+      render :action => "new"
+    end
   end
+
+
+  private
+
+  def event_params
+    params.require(:event).permit(:artist, :description, :price_low, :price_high, :event_date)
+  end
+
 end
