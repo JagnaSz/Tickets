5f8e5819127591e22126223c67bd0c54ff44704c
Agnieszka Szczurek <agnieszka.szczurek@miquido.com>
2017-01-21 13:55:21 +0100

    Added admin functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,11 @@
 class UsersController < ApplicationController
+
+  before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
+  before_action :correct_user,   only: [:edit, :update]
+  before_action :admin_user,     only: :destroy
+
   def index
+    @users = User.all
   end
 
   def new
@@ -23,6 +29,26 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User deleted"
+    redirect_to users_url
+  end
+
+
+  def logged_in_user
+    unless logged_in?
+      store_location
+      flash[:danger] = "Please log in."
+      redirect_to login_url
+    end
+  end
+
+
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_url) unless current_user?(@user)
+  end
 
   # Never trust parameters from the scary internet, only allow the white list through.
   def users_params
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170121091337) do
+ActiveRecord::Schema.define(version: 20170121102738) do
 
   create_table "events", force: :cascade do |t|
     t.string   "artist"
@@ -52,6 +52,7 @@ ActiveRecord::Schema.define(version: 20170121091337) do
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
     t.string   "remember_digest"
+    t.boolean  "admin"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
